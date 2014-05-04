7cbe16c30e944c9e99050525153167a951deb1d4
Zizaco <zizaco@gmail.com>
2014-05-04 13:03:38 -0300

    Added comments to tests.py

--- tests.py
+++ tests.py
@@ -10,15 +10,18 @@ from mock import Mock
 sys.modules['gtk']          = Mock(name='gtk_module')
 sys.modules['appindicator'] = Mock(name='appindicator_module')
 
+# Import src modules
 from src.newrelic_indicator import *
 from src.newrelic_app import *
 from src.config_parser import *
 
+# Load test files
 test_files = glob.glob("tests/"+os.path.dirname(__file__)+"/*.py")
 
 for files in test_files:
     execfile(files)
 
+# Run test
 if __name__ == '__main__':
     unittest.main()
 
