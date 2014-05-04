14fabdcfcc2fbf6ebb723ef7674769cbaeb057e8
Zizaco <zizaco@gmail.com>
2014-05-03 22:47:44 -0300

    Added NewrelicApp class in order to handle data retrieval from NewRelic API

---
+++ src/newrelic_app.py
@@ -0,0 +1,41 @@
+
+import json
+import httplib
+
+class NewrelicApp:
+
+    def __init__(self, api_key, api_id):
+        self.api_key = api_key
+        self.api_id  = api_id
+        self.raw_app = {}
+        self.reachable = False
+
+    def update_state(self):
+        headers = {"X-Api-Key":self.api_key}
+        url     ='/v2/applications/'+self.api_id+'.json'
+        conn    = httplib.HTTPConnection('api.newrelic.com')
+
+        conn.connect()
+        conn.request('GET', url, '', headers)
+        response = conn.getresponse()
+        if response.status == 200:
+            self.raw_app = json.loads(response.read())
+            self.reachable = True
+        else:
+            self.reachable = False
+
+    def is_online(self):
+        return self.reachable
+
+    def get_rpm(self):
+        return self.raw_app['application']['application_summary']['throughput'] if self.is_online() else False
+
+    def get_response_time(self):
+        return self.raw_app['application']['application_summary']['response_time'] if self.is_online() else False
+
+    def get_errors(self):
+        return self.raw_app['application']['application_summary']['error_rate'] if self.is_online() else False
+
+    def get_info(self):
+        return ("{0:.0f}".format(self.get_response_time())+"ms | "+str(self.get_rpm())+" rpm | "+"{0:.0f}".format(self.get_errors())+" errors") if self.is_online() else "unavailable info"
+
---
+++ tests/newrelic_app_test.py
@@ -0,0 +1,129 @@
+from mock import Mock
+
+class NewrelicAppTest(unittest.TestCase):
+
+    def test_should_instantiate(self):
+        # Set
+        app = NewrelicApp('1111','777')
+
+        # Assertion
+        self.assertIsInstance(app, NewrelicApp)
+        self.assertEquals(app.api_key,'1111')
+        self.assertEquals(app.api_id,'777')
+        self.assertFalse(app.reachable)
+
+    def test_should_update_state_if_available(self):
+        # Set
+        app = NewrelicApp('1111','777')
+        headers = {"X-Api-Key":'1111'}
+        url     ='/v2/applications/777.json'
+
+        # Mocks
+        connection_obj         = Mock()
+        response_obj           = Mock()
+        httplib.HTTPConnection = Mock(return_value=connection_obj)
+
+        response_obj.status        = 200
+        response_obj.read          = Mock(return_value='{"some":"json"}')
+        connection_obj.connect     = Mock()
+        connection_obj.request     = Mock()
+        connection_obj.getresponse = Mock(return_value=response_obj)
+
+        # Assertion
+        app.update_state()
+
+        connection_obj.connect.assert_called_once()
+        connection_obj.request.assert_called_once_with('GET', url, '', headers)
+        connection_obj.getresponse.assert_called_once()
+
+        self.assertEquals(app.raw_app, {"some":"json"})
+        self.assertTrue(app.reachable)
+
+    def test_should_not_update_state_if_unaavailable(self):
+        # Set
+        app = NewrelicApp('1111','777')
+        headers = {"X-Api-Key":'1111'}
+        url     ='/v2/applications/777.json'
+
+        # Mocks
+        connection_obj         = Mock()
+        response_obj           = Mock()
+        httplib.HTTPConnection = Mock(return_value=connection_obj)
+
+        response_obj.status        = 500 # Error
+        response_obj.read          = Mock(return_value='')
+        connection_obj.connect     = Mock()
+        connection_obj.request     = Mock()
+        connection_obj.getresponse = Mock(return_value=response_obj)
+
+        # Assertion
+        app.update_state()
+
+        connection_obj.connect.assert_called_once()
+        connection_obj.request.assert_called_once_with('GET', url, '', headers)
+        connection_obj.getresponse.assert_called_once()
+
+        self.assertFalse(app.reachable)
+
+    def test_should_check_is_online(self):
+        # Set
+        app = NewrelicApp('1111','777')
+
+        # Assert
+        app.reachable = True
+        self.assertTrue(app.is_online())
+
+        app.reachable = False
+        self.assertFalse(app.is_online())
+
+    def test_should_get_rpm(self):
+        # Set
+        app = NewrelicApp('1111','777')
+        app.raw_app = {'application':{'application_summary':{'throughput':3000}}}
+
+        # Assert
+        app.reachable = True
+        self.assertEquals(3000, app.get_rpm())
+
+        app.reachable = False
+        self.assertFalse(app.get_rpm())
+
+    def test_should_get_response_time(self):
+        # Set
+        app = NewrelicApp('1111','777')
+        app.raw_app = {'application':{'application_summary':{'response_time':100}}}
+
+        # Assert
+        app.reachable = True
+        self.assertEquals(100, app.get_response_time())
+
+        app.reachable = False
+        self.assertFalse(app.get_response_time())
+
+    def test_should_get_errors(self):
+        # Set
+        app = NewrelicApp('1111','777')
+        app.raw_app = {'application':{'application_summary':{'error_rate':3}}}
+
+        # Assert
+        app.reachable = True
+        self.assertEquals(3, app.get_errors())
+
+        app.reachable = False
+        self.assertFalse(app.get_errors())
+
+    def test_should_get_info(self):
+        # Set
+        app = NewrelicApp('1111','777')
+
+        # Mocks
+        app.get_rpm           = Mock(return_value=3000)
+        app.get_response_time = Mock(return_value=85.9)
+        app.get_errors        = Mock(return_value=3.001)
+
+        # Asset
+        app.reachable = True
+        self.assertEquals('86ms | 3000 rpm | 3 errors', app.get_info())
+
+        app.reachable = False
+        self.assertEquals('unavailable info', app.get_info())
